531ac5823546f6f30f01d6b2a21b36dea6e7091f
Jspback <bekciyusuf336@gmail.com>
2025-02-03 00:02:56 +0300

    utils update

--- src/main.zig
+++ src/main.zig
@@ -8,83 +8,17 @@ const sha512 = @import("algo.zig").sha512;
 const blake256 = @import("algo.zig").blake256;
 
 const toHex = @import("utils.zig").toHex;
+const parseArgs = @import("utils.zig").parseArgs;
 
 pub fn main() !void {
     var allocator = std.heap.page_allocator;
     const argv = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, argv);
 
-    var input: ?[]const u8 = null;
-    var algo: ?[]const u8 = null;
-    var readFromFile = false;
-    var compareHash: ?[]const u8 = null;
+    const options = try parseArgs(&allocator, argv);
 
-    var i: usize = 1;
-    while (i < argv.len) : (i += 1) {
-        const arg = argv[i];
-        if (std.mem.eql(u8, arg, "--list") or std.mem.eql(u8, arg, "ls")) {
-            std.debug.print("Usage:\n  {s} <input> [--algo|-a <algorithm>] [--file|-f <file loc>] [--compare|-c <hash>]\n\nSupported algorithms:\n  rot13   : Apply ROT13 cipher (not for files)\n  reverse : Reverse the input (not for files)\n  upper   : Convert letters to uppercase (not for files)\n  sha256  : Compute SHA-256 hash (32-byte output in hex)\n  sha512  : Compute SHA-512 hash (64-byte output in hex)\n  blake256: Compute BLAKE2s-256 hash (32-byte output in hex)\n", .{argv[0]});
-            return;
-        } else if (std.mem.eql(u8, arg, "--compare") or std.mem.eql(u8, arg, "-c")) {
-            i += 1;
-            if (i >= argv.len) {
-                std.debug.print("Error: missing value for --compare flag\n", .{});
-                return;
-            }
-            compareHash = argv[i];
-        } else if (std.mem.eql(u8, arg, "--algo") or std.mem.eql(u8, arg, "-a")) {
-            i += 1;
-            if (i >= argv.len) {
-                std.debug.print("Error: missing value for --algo flag\n", .{});
-                return;
-            }
-            algo = argv[i];
-        } else if (std.mem.eql(u8, arg, "--file") or std.mem.eql(u8, arg, "-f")) {
-            i += 1;
-            if (i >= argv.len) {
-                std.debug.print("Error: missing value for --file flag\n", .{});
-                return;
-            }
-
-            const file = try std.fs.cwd().openFile(argv[i], .{});
-            defer file.close();
-
-            const file_size = (try file.stat()).size;
-
-            const buffer = try allocator.alloc(u8, file_size);
-            defer allocator.free(buffer);
-
-            try file.reader().readNoEof(buffer);
-            input = buffer;
-            readFromFile = true;
-        } else {
-            if (input != null) {
-                // that means we have multiple inputs (probably input with spaces) so we merge them into one
-                const u_input = input.?;
-                const new_len = u_input.len + 1 + arg.len;
-                var concatenated = try allocator.alloc(u8, new_len);
-                std.mem.copyForwards(u8, concatenated[0..u_input.len], u_input);
-                concatenated[u_input.len] = ' ';
-                std.mem.copyForwards(u8, concatenated[u_input.len + 1 .. new_len], arg);
-                input = concatenated;
-            } else {
-                input = arg;
-            }
-        }
-    }
-    if (input == null) {
-        std.debug.print("Usage:\n  {s} <input> [--algo|-a <algorithm>]\nUse '--list' to see supported algorithms.\n", .{argv[0]});
-        return;
-    }
-    if (algo == null) {
-        algo = "sha256";
-    }
-    const inStr = input.?;
-    const selectedAlgo = algo.?;
-    if (readFromFile and !(std.mem.eql(u8, selectedAlgo, "sha256") or std.mem.eql(u8, selectedAlgo, "sha512") or std.mem.eql(u8, selectedAlgo, "blake256"))) {
-        std.debug.print("Error: only sha256, sha512, blake256 are allowed with --file\n", .{});
-        return;
-    }
+    const inStr = options.input;
+    const selectedAlgo = options.algo;
 
     var outLen: usize = 0;
     var hashOutput: bool = false;
@@ -97,7 +31,10 @@ pub fn main() !void {
     } else if (std.mem.eql(u8, selectedAlgo, "blake256")) {
         outLen = 32;
         hashOutput = true;
-    } else if (std.mem.eql(u8, selectedAlgo, "rot13") or std.mem.eql(u8, selectedAlgo, "reverse") or std.mem.eql(u8, selectedAlgo, "upper")) {
+    } else if (std.mem.eql(u8, selectedAlgo, "rot13") or
+        std.mem.eql(u8, selectedAlgo, "reverse") or
+        std.mem.eql(u8, selectedAlgo, "upper"))
+    {
         outLen = inStr.len;
     } else {
         std.debug.print("Unrecognized algorithm: {s}\n", .{selectedAlgo});
@@ -122,10 +59,10 @@ pub fn main() !void {
     }
 
     if (hashOutput) {
-        const hexResult = try toHex(allocator, result);
+        const hexResult = try toHex(&allocator, result);
         defer allocator.free(hexResult);
-        if (compareHash != null) {
-            if (std.mem.eql(u8, hexResult, compareHash.?)) {
+        if (options.compareHash != null) {
+            if (std.mem.eql(u8, hexResult, options.compareHash.?)) {
                 std.debug.print("Hashes match\n", .{});
             } else {
                 std.debug.print("Hashes differ\n", .{});
--- src/utils.zig
+++ src/utils.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
 
-pub fn toHex(allocator: std.mem.Allocator, bytes: []const u8) ![]u8 {
+const Options = struct {
+    input: []const u8,
+    algo: []const u8,
+    readFromFile: bool,
+    compareHash: ?[]const u8,
+};
+
+pub fn toHex(allocator: *std.mem.Allocator, bytes: []const u8) ![]u8 {
     const hexDigits = "0123456789abcdef";
     const hexLen = bytes.len * 2;
     var hexBuffer = try allocator.alloc(u8, hexLen);
@@ -11,4 +18,72 @@ pub fn toHex(allocator: std.mem.Allocator, bytes: []const u8) ![]u8 {
         j += 2;
     }
     return hexBuffer;
+}
+
+pub fn parseArgs(allocator: *std.mem.Allocator, argv: [][]const u8) !Options {
+    var opt = Options{
+        .input = "",
+        .algo = "sha256",
+        .readFromFile = false,
+        .compareHash = null,
+    };
+
+    var i: usize = 1;
+    while (i < argv.len) : (i += 1) {
+        const arg = argv[i];
+        if (std.mem.eql(u8, arg, "--list") or std.mem.eql(u8, arg, "ls")) {
+            std.debug.print("Usage:\n  {s} <input> [--algo|-a <algorithm>] [--file|-f <file loc>] [--compare|-c <hash>]\n\nSupported algorithms:\n  rot13   : Apply ROT13 cipher (not for files)\n  reverse : Reverse the input (not for files)\n  upper   : Convert letters to uppercase (not for files)\n  sha256  : Compute SHA-256 hash (32-byte output in hex)\n  sha512  : Compute SHA-512 hash (64-byte output in hex)\n  blake256: Compute BLAKE2s-256 hash (32-byte output in hex)\n", .{argv[0]});
+            return error.InvalidUsage;
+        } else if (std.mem.eql(u8, arg, "--compare") or std.mem.eql(u8, arg, "-c")) {
+            i += 1;
+            if (i >= argv.len) {
+                std.debug.print("Error: missing value for --compare flag\n", .{});
+                return error.InvalidUsage;
+            }
+            opt.compareHash = argv[i];
+        } else if (std.mem.eql(u8, arg, "--algo") or std.mem.eql(u8, arg, "-a")) {
+            i += 1;
+            if (i >= argv.len) {
+                std.debug.print("Error: missing value for --algo flag\n", .{});
+                return error.InvalidUsage;
+            }
+            opt.algo = argv[i];
+        } else if (std.mem.eql(u8, arg, "--file") or std.mem.eql(u8, arg, "-f")) {
+            i += 1;
+            if (i >= argv.len) {
+                std.debug.print("Error: missing value for --file flag\n", .{});
+                return error.InvalidUsage;
+            }
+
+            const file = try std.fs.cwd().openFile(argv[i], .{});
+            defer file.close();
+
+            const file_size = (try file.stat()).size;
+
+            const buffer = try allocator.alloc(u8, file_size);
+            defer allocator.free(buffer);
+
+            try file.reader().readNoEof(buffer);
+            opt.input = buffer;
+            opt.readFromFile = true;
+        } else {
+            if (!std.mem.eql(u8, opt.input, "")) {
+                // that means we have multiple inputs (probably input with spaces) so we merge them into one
+                const u_input = opt.input;
+                const new_len = u_input.len + 1 + arg.len;
+                var concatenated = try allocator.alloc(u8, new_len);
+                std.mem.copyForwards(u8, concatenated[0..u_input.len], u_input);
+                concatenated[u_input.len] = ' ';
+                std.mem.copyForwards(u8, concatenated[u_input.len + 1 .. new_len], arg);
+                opt.input = concatenated;
+            } else {
+                opt.input = arg;
+            }
+        }
+    }
+    if (std.mem.eql(u8, opt.input, "")) {
+        std.debug.print("Usage:\n  {s} <input> [--algo|-a <algorithm>]\nUse '--list' to see supported algorithms.\n", .{argv[0]});
+        return error.InvalidUsage;
+    }
+    return opt;
 }
